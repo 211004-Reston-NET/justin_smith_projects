c3a2f4d9d42c209f721369f6919b68cc7424473a
jlsmith497 <jlsmith497@gmail.com>
2021-10-18 15:50:33 -0400

    Store Project Added

---
+++ justin-smith-p0/SFBL/IStoreBL.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SFBL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
